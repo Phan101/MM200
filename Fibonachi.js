344834aade33f649f04006d139e4758cc2e6a6bc
Morten <89643277+Mortenrein99@users.noreply.github.com>
2021-11-17 14:52:45 +0100

    Add files via upload

---
+++ Fibonachi.js
@@ -0,0 +1,13 @@
+
+const number = process.argv[2];
+let n1 = 0, n2 = 1, nxtNumber;
+
+console.log('Fibonacci Series:');
+
+for (let i = 1; i <= number; i++) {
+    console.log(n1);
+    nxtNumber = n1 + n2;
+    n1 = n2;
+    n2 = nxtNumber;
+}
+
